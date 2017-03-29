eb2f1d719c7dec85ebbbf375c4c7a26f74aad3f0
Osbert Orr <osbert.orr@digitalinsight.com>
2017-03-28 18:42:05 -0700

    Update solution to problem

--- leetcode/q325-maximum-size-subarray-sum-equals-k.js
+++ leetcode/q325-maximum-size-subarray-sum-equals-k.js
@@ -6,16 +6,18 @@
  */
 var maxSubArrayLen = function(nums, k) {
     let max = 0;
-    let map = {};
-    for(let i = 0, sum = 0, l = nums.length; i < l; i++) {
+    for(let map = new Map(), l = nums.length, sum = 0, i = 0; i < l; i++) {
         sum += nums[i];
+        
         if(sum === k) {
             max = i + 1;
-        } else if(map[sum - k] !== undefined) {
-            max = Math.max(max, i - map[sum - k]);
+        } else if(map.get(sum - k) !== undefined) {
+            max = Math.max(max, i - map.get(sum - k));
         }
         
-        if(map[sum] === undefined) map[sum] = i;
+        if(map.get(sum) === undefined) {
+            map.set(sum, i);
+        }
     }
     
     return max;
